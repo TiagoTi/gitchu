db156e06f0641375210850ed718142a4d8900593
tiagoti <tiagoandroidti@gmail.com>
2019-07-20 15:14:15 -0300

    add ssh key-name to config

--- gitchu.sh
+++ gitchu.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-VERSION=0.0.1
+VERSION=0.0.2
 HOME_GITHUB=$HOME/.gitchu
 
 info() {
     echo "Version: $VERSION"
 }
 
+add-key() {
+    echo "please account alias"
+    read ACCOUNT_ALIAS
+
+    echo "chose your key"
+    ls -la $HOME/.ssh
+    echo "chose your key"
+    read SSH_KEY
+
+    echo "please email address"
+    read ACCOUNT_EMAIL
+
+    echo "please your name to git repo"
+    read ACCOUNT_NICK
+
+    echo $ACCOUNT_ALIAS
+    echo $SSH_KEY
+    echo $ACCOUNT_EMAIL
+    echo $ACCOUNT_NICK
+
+    touch $HOME_GITHUB/$ACCOUNT_ALIAS
+
+    echo $ACCOUNT_ALIAS >$HOME_GITHUB/$ACCOUNT_ALIAS
+    echo $SSH_KEY >>$HOME_GITHUB/$ACCOUNT_ALIAS
+    echo $ACCOUNT_EMAIL >>$HOME_GITHUB/$ACCOUNT_ALIAS
+    echo $ACCOUNT_NICK >>$HOME_GITHUB/$ACCOUNT_ALIAS
+}
+
 case "$1" in
 --info)
     info
+    ;;
+--add-key)
+    add-key
     ;;
 *)
     info
