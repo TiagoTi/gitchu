2e1dd91c951d45c5dae6d5f9b9854731a0180c48
TiagoTi <tiago@ti2.net.br>
2019-07-23 06:21:35 -0300

    use function

--- gitchu.sh
+++ gitchu.sh
@@ -35,12 +35,39 @@ add-key() {
     echo $ACCOUNT_NICK >>$HOME_GITHUB/$ACCOUNT_ALIAS
 }
 
+use() {
+    echo "chose a config : "
+    ls -1 $HOME_GITHUB | grep -v gitch
+    read CONFIGURATION
+
+    ACCOUNT_ALIAS=$(cat $HOME_GITHUB/$CONFIGURATION | sed -n '1p')
+    SSH_KEY=$(cat $HOME_GITHUB/$CONFIGURATION | sed -n '2p')
+    ACCOUNT_EMAIL=$(cat $HOME_GITHUB/$CONFIGURATION | sed -n '3p')
+    ACCOUNT_NICK=$(cat $HOME_GITHUB/$CONFIGURATION | sed -n '4p')
+
+    echo $ACCOUNT_ALIAS
+    echo $SSH_KEY
+    echo $ACCOUNT_EMAIL
+    echo $ACCOUNT_NICK
+
+    echo "[user]" >$HOME/.gitconfig
+    echo "    name = $ACCOUNT_NICK" >>$HOME/.gitconfig
+    echo "    email = $ACCOUNT_EMAIL" >>$HOME/.gitconfig
+    cat $HOME/.gitconfig
+
+    ssh-add -D
+    ssh-add ~/.ssh/$SSH_KEY
+}
+
 case "$1" in
 --info)
     info
     ;;
 --add-key)
     add-key
+    ;;
+--use)
+    use
     ;;
 *)
     info
